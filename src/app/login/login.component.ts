f9e7df963000c1f0e7c6f54b60c78619a87d33e9
janis.tejero <janis.tejero@stud.bbbaden.ch>
2019-12-28 17:31:01 +0100

    calculation and display of statistical values

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
     var user = new User(this.username, this.password, "");
     this.coreService.loginUser(user).toPromise().then((data: any) => {
       var dat = data;
-      console.log(dat)
+      console.log(dat.user)
       if (dat != undefined) {
         var message = dat.response;
         if (message == "Success") {
--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { NbDialogService } from '@nebular/theme';
 import { UserService } from "../api/user.service";
+import { CoreService } from "../api/core.service";
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
     selector: 'app-statistics',
@@ -10,32 +12,67 @@ import { UserService } from "../api/user.service";
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class StatisticsComponent implements OnInit{
+export class StatisticsComponent implements OnInit {
 
-    constructor(private userService: UserService){}
+    userdata
 
-    ngOnInit() {}
+    constructor(private userService: UserService, private coreService: CoreService) { this.userdata = this.coreService.getUserInformation(); }
+    ngOnInit() { }
 
 
-    getPerformance(){
+    // return current portfolio return
+    getPortfolioReturn() {
+        var netprofit = this.userdata['portfolio']['currentValue'] - this.userdata['portfolio']['startCapital']
+        netprofit = Math.round((netprofit + Number.EPSILON) * 100) / 100
+        return Math.round(((netprofit / this.userdata['portfolio']['startCapital']) * 100 + Number.EPSILON) * 100) / 100 + ' %'
+    }
+
+    // return current return on investment
+    getROI() {
         // ROI = ((Cv - COI) / COI) * 100
-        // Rp = NP / P 
+        var netprofit = this.userdata['portfolio']['currentValue'] - this.userdata['portfolio']['startCapital']
+        netprofit = Math.round((netprofit + Number.EPSILON) * 100) / 100
+
+        var transactions = this.userService.GetUserTransactions()
+
+        // sum of the $ paid for all stocks
+        var totalPaid = 0
+        var i;
+        for (i = 0; i < transactions.length; i++) {
+            totalPaid += Math.round((parseFloat(transactions[i]['data']['totalValue']) + Number.EPSILON) * 100) / 100
+        }
+        return Math.round(((netprofit / (totalPaid)) * 100 + Number.EPSILON) * 100) / 100 + ' %'
     }
 
-    getCash(){
-        // return balance
-        var portfolioitems = this.userService.getPortfolioItems()
-        console.log(portfolioitems)
-        console.log(portfolioitems[0])
-        return "drei"
+    // return balance from cookie
+    getCash() {
+        var currentBalance = this.userdata['portfolio']['currentBalance']
+        var decimalStr = currentBalance.toString().split(".")[1]
+        var newFirstDigit = (parseInt(decimalStr[1]) >= 5) ? "5" : "0"
+        currentBalance = currentBalance.toString().split(".")[0] + '.' + decimalStr[0] + newFirstDigit
+        return this.formatToUSD(currentBalance);
     }
 
-    getMarketValue(){
-        // return current value from cookie
+    // return current value from cookie
+    getMarketValue() {
+        return this.formatToUSD(this.userdata['portfolio']['currentValue'])
+    }
+
+    getTradesMade() {
+        var transactions = this.userService.GetUserTransactions()
+        return transactions.length
     }
 
     public performanceChartOption = {
         scaleShowVerticalLines: false,
         responsive: true
     }
- }
+
+    formatToUSD = function (val) {
+        var formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        });
+        return formatter.format(val);
+    }
+}
